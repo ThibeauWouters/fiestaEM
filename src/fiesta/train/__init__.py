3344f4925a4285e5d1c83b2b1fb8a31e7100af2e
haukekoehn <hauke.koehn@uni-potsdam.de>
2026-09-08 17:44:35 +0200

    Small editorial changes to Benchmarker

--- src/fiesta/train/__init__.py
+++ src/fiesta/train/__init__.py
@@ -1,4 +1,5 @@
 from .DataManager import DataManager
 from .FluxTrainer import PCATrainer, CVAETrainer
+from .Benchmarker import Benchmarker
 from .neuralnets import CVAE, MLP, NeuralnetConfig
 from .utils import train_test_split, write_training_data
